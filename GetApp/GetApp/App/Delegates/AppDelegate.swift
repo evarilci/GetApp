64497a8fc92f0bda0a77e301417c513de90eac60
Eymen Varilci <evarilci@gmail.com>
2022-10-26 22:30:17 +0300

    Initial Commit

---
+++ GetApp/GetApp/App/Delegates/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GetApp
+//
+//  Created by Eymen Varilci on 26.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ GetApp/GetApp/Screens/Inıtial Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetApp
+//
+//  Created by Eymen Varilci on 26.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
