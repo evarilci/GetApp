cf75e803f59cea19b4b53e3ca92d3d87e2ace135
Eymen Varilci <evarilci@gmail.com>
2022-11-05 02:43:04 +0300

    fetching data to local works. Need to usi it from it.

--- GetApp/GetApp/Screens/Detail/ProductEntity/ProductEntity.swift
+++ GetApp/GetApp/Screens/Detail/ProductEntity/ProductEntity.swift
@@ -10,8 +10,17 @@ import RealmSwift
 
 
 class ProductEntity: Object {
-    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted(primaryKey: true) var _id: Int
     @Persisted var title: String
     @Persisted var price: Double
     @Persisted var desc: String
+    @Persisted var category: String
+    @Persisted var image: String
+    @Persisted var rating: RatingEntity?
+}
+
+class RatingEntity: Object {
+    
+    @Persisted var rate: Double
+    @Persisted var count: Int
 }
--- GetApp/GetApp/Screens/Home/ViewModel/HomeViewModel.swift
+++ GetApp/GetApp/Screens/Home/ViewModel/HomeViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Moya
 
+
 protocol HomeViewModelDelegate: AnyObject {
     
     func errorOcurred(_ error: Error)
@@ -24,7 +25,7 @@ protocol HomeViewModelProtocol {
     func fetchProduct()
 }
 
-final class HomeViewModel: HomeViewModelProtocol {
+final class HomeViewModel: HomeViewModelProtocol  {
     
     weak var delegate: HomeViewModelDelegate?
     
@@ -61,6 +62,29 @@ final class HomeViewModel: HomeViewModelProtocol {
                 do {
                     let products = try JSONDecoder().decode([Product].self, from: response.data)
                     self?.products = products
+                    products.forEach { product in
+                        guard self!.realm.object(ofType: ProductEntity.self, forPrimaryKey: product.id) == nil else {return}
+                        
+                        let productEntity = ProductEntity()
+                        productEntity._id = product.id ?? .zero
+                        productEntity.title = product.title!
+                        productEntity.price = product.price!
+                        productEntity.category = product.category!
+                        productEntity.image = product.image!
+                       
+                        let ratingEntity = RatingEntity()
+                        ratingEntity.rate = product.rating!.rate!
+                        ratingEntity.count = product.rating!.count!
+                        productEntity.rating = ratingEntity
+                        do {
+                            try self!.realm.write {
+                                self!.realm.add(productEntity)
+                            }
+                        } catch {
+                            self?.delegate?.errorOcurred(error)
+                        }
+                    }
+                   
                     self?.delegate?.fetchSucceded()
                 } catch  {
                     self?.delegate?.errorOcurred(error)
@@ -68,4 +92,12 @@ final class HomeViewModel: HomeViewModelProtocol {
             }
         }
     }
+    
+    func parseProduct() {
+        
+    }
+}
+
+extension HomeViewModel: RealmReachable {
+    
 }
